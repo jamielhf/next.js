5a681d9ff0dd128238d976222db6798762a88177
linhaifeng <linhaifeng@yy.com>
2020-08-21 15:19:37 +0800

    update

---
+++ pages/index.jsx
@@ -0,0 +1,23 @@
+import { Form, Select, InputNumber, Switch, Slider, Button } from 'antd'
+
+// Custom DatePicker that uses Day.js instead of Moment.js
+import DatePicker from '../components/DatePicker'
+
+import { SmileFilled } from '@ant-design/icons'
+
+import Link from 'next/link'
+
+const FormItem = Form.Item
+const Option = Select.Option
+
+const content = {
+  marginTop: '100px',
+}
+
+export default function Home() {
+  return (
+    <div style={content}>
+      123
+    </div>
+  )
+}
